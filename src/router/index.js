0ad3e08d71af1881c43b5c16015a9fab7c49a129
zhb <2032973256@qq.com>
2019-12-03 09:41:44 +0800

    study1

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,6 @@ const routes = [
   {
     path: '/detail',
     name: 'detail',
-    
     component: () => import('../views/detail.vue')
   }
 ]
--- src/views/detail.vue
+++ src/views/detail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div >detail</div>
+  <div class="detail">detail</div>
 </template>
 
 <script>
@@ -8,6 +8,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.detail{
+  width:100%;
+  height:100%;
+}
 </style>
